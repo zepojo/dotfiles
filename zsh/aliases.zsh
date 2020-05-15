369ffc5e32689c3965a2cdb653d8a27c9cab9b92
ink-spot <ulric.paul@gmail.com>
2020-05-16 00:39:34 +0200

    Add zsh aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,14 @@
 alias reload!='. ~/.zshrc'
 
+alias aliases="vi ~/.dotfiles/zsh/aliases.zsh"
+
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias h="cd ~"
+alias ..="cd .."
+alias ...="cd ../.."
+alias la="ls -al"
+alias ll="ls -l"
+alias l="ls -al"
+
+alias ifind="find . -name"
