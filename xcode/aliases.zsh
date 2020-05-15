25a71d223b37561ec3cb9542e57112ca34305b28
ink-spot <ulric.paul@gmail.com>
2020-05-16 00:41:19 +0200

    Add Xcode aliases

--- xcode/aliases.zsh
+++ xcode/aliases.zsh
@@ -1,2 +1,3 @@
 alias ios="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator.app"
 alias watchos="open /Applications/Xcode.app/Contents/Developer/Applications/Simulator\ \(Watch\).app"
+alias cleandd="rm -rf ~/Library/Developer/Xcode/DerivedData"
